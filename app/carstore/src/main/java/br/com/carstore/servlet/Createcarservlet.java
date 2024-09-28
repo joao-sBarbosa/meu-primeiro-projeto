277639b229f106be13177abca1c3e7360ffdc0d4
JOAO VICTOR BARBOSA DA SILVA <joaovictorbarbosa765@yahoo.com>
2024-09-27 21:32:45 -0300

    banco de dados no meu primeiro projeto

--- app/carstore/src/main/java/br/com/carstore/servlet/Createcarservlet.java
+++ app/carstore/src/main/java/br/com/carstore/servlet/Createcarservlet.java
@@ -1,5 +1,8 @@
 package br.com.carstore.servlet;
 
+import br.com.carstore.dao.CarDao;
+import br.com.carstore.model.Car;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,8 +18,18 @@ public class Createcarservlet extends HttpServlet{
 
 
         String carName = req.getParameter("car-name");
+        String carcolor = req.getParameter("car-color");
 
         System.out.println(carName);
+        System.out.println(carcolor);
         req.getRequestDispatcher("index.html").forward(req, resp);
+
+        Car car = new Car();
+        CarDao cardao = new CarDao();
+
+        car.setName(carName);
+        car.setColor(carcolor);
+
+        cardao.createCar(car);
     }
 }
